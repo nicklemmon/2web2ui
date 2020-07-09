ac129d2e07e2767ad73e3ed68d441589a3a839bb
shrirupa <shrirupa.chowdhury@sparkpost.com>
2020-07-09 15:16:16 -0400

    [AC-1325] Removed the priceInfo object

--- src/components/billing/PlanPrice.js
+++ src/components/billing/PlanPrice.js
@@ -19,15 +19,6 @@ const PlanPrice = ({
   if (_.isEmpty(plan)) {
     return null;
   }
-  const getPlanPrice = plan => {
-    return {
-      intervalShort: 'mo',
-      intervalLong: 'monthly',
-      price: plan.price,
-    };
-  };
-
-  const priceInfo = getPlanPrice(plan);
 
   const overage =
     plan.price <= 0 || plan.isFree
@@ -40,17 +31,17 @@ const PlanPrice = ({
 
   const displayCsm = showCsm && plan.includesCsm;
 
-  let discountAmount = priceInfo.price;
+  let discountAmount = plan.price;
 
   if (selectedPromo.discount_amount) {
-    discountAmount = Math.max(priceInfo.price - selectedPromo.discount_amount, 0);
+    discountAmount = Math.max(plan.price - selectedPromo.discount_amount, 0);
   }
 
   if (selectedPromo.discount_percentage) {
     discountAmount = discountAmount * ((100 - selectedPromo.discount_percentage) / 100);
   }
 
-  const hasDiscount = discountAmount !== priceInfo.price;
+  const hasDiscount = discountAmount !== plan.price;
 
   return (
     <span className={cx('notranslate', className)}>
@@ -61,12 +52,11 @@ const PlanPrice = ({
           </Text>
         </strong>
         <span> emails/month </span>
-        {priceInfo.price > 0 ? (
+        {plan.price > 0 ? (
           <span>
             {' at '}
-            {hasDiscount && <s className={styles.DiscountedLabel}>${priceInfo.price}</s>}
-            <strong>{hasDiscount ? formatCurrency(discountAmount) : `$${priceInfo.price}`}</strong>/
-            {priceInfo.intervalShort}
+            {hasDiscount && <s className={styles.DiscountedLabel}>${plan.price}</s>}
+            <strong>{hasDiscount ? formatCurrency(discountAmount) : `$${plan.price}`}</strong>/mo
           </span>
         ) : (
           <span> FREE </span>
