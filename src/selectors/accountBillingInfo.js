9738a4489e1e4f693257a6e790506949d5d91d10
shrirupa <shrirupa.chowdhury@sparkpost.com>
2020-07-21 10:08:50 -0400

    [AC-1557] hasOnlineSupport createSelector

--- src/selectors/accountBillingInfo.js
+++ src/selectors/accountBillingInfo.js
@@ -27,7 +27,9 @@ const selectIsCcFree1 = selectCondition(onPlan('ccfree1'));
 const selectIsFree1 = selectCondition(onPlan('free1'));
 const selectOnZuoraPlan = selectCondition(onZuoraPlan);
 const hasDedicatedIpsOnSubscription = selectCondition(hasProductOnSubscription('dedicated_ip'));
-export const hasOnlineSupport = selectCondition(hasProductOnSubscription('online_support'));
+const selectOnlineSupportOnSubscription = selectCondition(
+  hasProductOnSubscription('online_support'),
+);
 const selectPhoneSupportOnSubscription = selectCondition(hasProductOnSubscription('phone_support'));
 const selectBillingSubscription = state => state.billing.subscription || {};
 const currentFreePlans = ['free500-1018', 'free15K-1018', 'free500-0419', 'free500-SPCEU-0419'];
@@ -37,6 +39,10 @@ export const currentSubscriptionSelector = state => state.account.subscription;
 export const hasPhoneSupport = createSelector(
   [selectPhoneSupportOnSubscription],
   hasPhoneSupport => hasPhoneSupport,
+);
+export const hasOnlineSupport = createSelector(
+  [selectOnlineSupportOnSubscription],
+  hasOnlineSupport => hasOnlineSupport,
 );
 /**
  * Returns current subscription's code
