2f9034e08622e3372044aac81ab617ce2032f677
shrirupa <shrirupa.chowdhury@sparkpost.com>
2020-07-22 13:00:37 -0400

    [AC-1557] AC-1557 - removing duplicate selectors

--- src/selectors/accountBillingInfo.js
+++ src/selectors/accountBillingInfo.js
@@ -27,23 +27,13 @@ const selectIsCcFree1 = selectCondition(onPlan('ccfree1'));
 const selectIsFree1 = selectCondition(onPlan('free1'));
 const selectOnZuoraPlan = selectCondition(onZuoraPlan);
 const hasDedicatedIpsOnSubscription = selectCondition(hasProductOnSubscription('dedicated_ip'));
-const selectOnlineSupportOnSubscription = selectCondition(
-  hasProductOnSubscription('online_support'),
-);
-const selectPhoneSupportOnSubscription = selectCondition(hasProductOnSubscription('phone_support'));
+export const hasOnlineSupport = selectCondition(hasProductOnSubscription('online_support'));
+export const hasPhoneSupport = selectCondition(hasProductOnSubscription('phone_support'));
 const selectBillingSubscription = state => state.billing.subscription || {};
 const currentFreePlans = ['free500-1018', 'free15K-1018', 'free500-0419', 'free500-SPCEU-0419'];
 export const isManuallyBilled = state => _.get(state, 'billing.subscription.type') === 'manual';
 const getRecipientValidationUsage = state => _.get(state, 'account.rvUsage.recipient_validation');
 export const currentSubscriptionSelector = state => state.account.subscription;
-export const hasPhoneSupport = createSelector(
-  [selectPhoneSupportOnSubscription],
-  hasPhoneSupport => hasPhoneSupport,
-);
-export const hasOnlineSupport = createSelector(
-  [selectOnlineSupportOnSubscription],
-  hasOnlineSupport => hasOnlineSupport,
-);
 /**
  * Returns current subscription's code
  * @param state
