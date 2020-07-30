fdd44cfe5f7b3f581e068b49478628571d02de22
shrirupa <shrirupa.chowdhury@sparkpost.com>
2020-07-30 11:41:00 -0400

    [AC-1595] removed call to getSubscription for heroku users

--- src/actions/accessControl.js
+++ src/actions/accessControl.js
@@ -1,6 +1,7 @@
 import { fetch as fetchAccount } from './account';
 import { get as getCurrentUser, getGrants } from './currentUser';
 import { getPlans, getBundles, getSubscription } from './billing';
+import { isHeroku, isAzure } from 'src/helpers/conditions/user';
 
 // initialize some state used for access control
 export function initializeAccessControl() {
@@ -8,14 +9,24 @@ export function initializeAccessControl() {
   // Hides global alerts when user is logged out and redirected to /auth
   const meta = { showErrorAlert: false };
 
-  return dispatch =>
-    Promise.all([
-      dispatch(fetchAccount({ meta })),
-      dispatch(getPlans({ meta })),
-      dispatch(getBundles({ meta })),
-      dispatch(getSubscription({ meta })),
-      dispatch(getCurrentUser({ meta })).then(({ access_level }) =>
+  return (dispatch, getState) => {
+    dispatch(getCurrentUser({ meta })).then(({ access_level }) => {
+      const allInitialCalls = [
         dispatch(getGrants({ role: access_level, meta })),
-      ),
-    ]).then(() => dispatch({ type: 'ACCESS_CONTROL_READY' }));
+        dispatch(fetchAccount({ meta })),
+        dispatch(getPlans({ meta })),
+        dispatch(getBundles({ meta })),
+      ];
+
+      if (isHeroku(getState()) || isAzure(getState())) {
+        return Promise.all([...allInitialCalls]).then(() =>
+          dispatch({ type: 'ACCESS_CONTROL_READY' }),
+        );
+      } else {
+        return Promise.all([...allInitialCalls, dispatch(getSubscription({ meta }))]).then(() =>
+          dispatch({ type: 'ACCESS_CONTROL_READY' }),
+        );
+      }
+    });
+  };
 }
--- src/actions/tests/accessControl.test.js
+++ src/actions/tests/accessControl.test.js
@@ -9,26 +9,43 @@ jest.mock('src/actions/billing');
 
 describe('Action: Initialize Access Control', () => {
   const meta = { showErrorAlert: false };
+  let dispatch;
+
   beforeEach(() => {
+    dispatch = jest.fn(a => Promise.resolve(a));
     fetchAccount.mockImplementation(() => Promise.resolve('test-account'));
     getPlans.mockImplementation(() => Promise.resolve('test-plans'));
     getBundles.mockImplementation(() => Promise.resolve('test-bundles'));
-    getCurrentUser.mockImplementation(() => Promise.resolve({ access_level: 'EQUISAPIEN' }));
+    getCurrentUser.mockImplementation(() => Promise.resolve({ access_level: 'admin' }));
     getGrants.mockImplementation(() => Promise.resolve('test-grants'));
     getSubscription.mockImplementation(() => Promise.resolve('test-subscription'));
   });
 
-  it('should initialize access control with a series of calls', async () => {
-    const dispatchMock = jest.fn(a => a);
-    await initializeAccessControl()(dispatchMock);
+  it('should initialize access control with a series of calls for non heroku and non azure users', async () => {
+    const thunk = initializeAccessControl();
+    const state = { currentUser: { access_level: 'admin' } };
 
+    await thunk(dispatch, () => state);
+    expect(getCurrentUser).toHaveBeenCalledWith({ meta });
+    expect(getGrants).toHaveBeenCalledWith({ role: 'admin', meta });
     expect(fetchAccount).toHaveBeenCalledWith({ meta });
     expect(getPlans).toHaveBeenCalledWith({ meta });
+    expect(getBundles).toHaveBeenCalledWith({ meta });
+    expect(getSubscription).toHaveBeenCalledWith({ meta });
+    expect(dispatch).toHaveBeenCalledTimes(6);
+  });
+  it('should initialize access control with a series of calls for heroku users', async () => {
+    getCurrentUser.mockImplementation(() => Promise.resolve({ access_level: 'heroku' }));
+    const thunk = initializeAccessControl();
+    const state = { currentUser: { access_level: 'heroku' } };
+
+    await thunk(dispatch, () => state);
     expect(getCurrentUser).toHaveBeenCalledWith({ meta });
-    expect(getGrants).toHaveBeenCalledWith({ role: 'EQUISAPIEN', meta });
-    expect(dispatchMock).toHaveBeenCalledTimes(7);
-    expect(dispatchMock).toHaveBeenLastCalledWith({
-      type: 'ACCESS_CONTROL_READY',
-    });
+    expect(getGrants).toHaveBeenCalledWith({ role: 'heroku', meta });
+    expect(fetchAccount).toHaveBeenCalledWith({ meta });
+    expect(getPlans).toHaveBeenCalledWith({ meta });
+    expect(getBundles).toHaveBeenCalledWith({ meta });
+    expect(getSubscription).not.toHaveBeenCalled();
+    expect(dispatch).toHaveBeenCalledTimes(5);
   });
 });
