51e74adf8534c7562f80a9a8028e302f3c547387
shrirupa <shrirupa.chowdhury@sparkpost.com>
2020-08-04 16:36:36 -0400

    [AC-1608] condition check for aws users + Snapshot updates :cry:

--- src/actions/accessControl.js
+++ src/actions/accessControl.js
@@ -2,6 +2,7 @@ import { fetch as fetchAccount } from './account';
 import { get as getCurrentUser, getGrants } from './currentUser';
 import { getPlans, getBundles, getSubscription } from './billing';
 import { isHeroku, isAzure } from 'src/helpers/conditions/user';
+import { isAws } from 'src/helpers/conditions/account';
 
 // initialize some state used for access control
 export function initializeAccessControl() {
@@ -10,21 +11,21 @@ export function initializeAccessControl() {
   const meta = { showErrorAlert: false };
 
   return (dispatch, getState) =>
-    dispatch(getCurrentUser({ meta })).then(({ access_level }) => {
-      const allInitialCalls = [
-        dispatch(getGrants({ role: access_level, meta })),
-        dispatch(fetchAccount({ meta })),
-        dispatch(getPlans({ meta })),
-        dispatch(getBundles({ meta })),
-      ];
-
-      if (isHeroku(getState()) || isAzure(getState())) {
-        return Promise.all([...allInitialCalls]).then(() =>
+    Promise.all([dispatch(getCurrentUser({ meta })), dispatch(fetchAccount({ meta }))]).then(
+      ([currentUser]) => {
+        const allInitialCalls = [
+          dispatch(getGrants({ role: currentUser.access_level, meta })),
+          dispatch(getPlans({ meta })),
+          dispatch(getBundles({ meta })),
+        ];
+        if (isHeroku(getState()) || isAzure(getState()) || isAws(getState())) {
+          return Promise.all([...allInitialCalls]).then(() =>
+            dispatch({ type: 'ACCESS_CONTROL_READY' }),
+          );
+        }
+        return Promise.all([...allInitialCalls, dispatch(getSubscription({ meta }))]).then(() =>
           dispatch({ type: 'ACCESS_CONTROL_READY' }),
         );
-      }
-      return Promise.all([...allInitialCalls, dispatch(getSubscription({ meta }))]).then(() =>
-        dispatch({ type: 'ACCESS_CONTROL_READY' }),
-      );
-    });
+      },
+    );
 }
