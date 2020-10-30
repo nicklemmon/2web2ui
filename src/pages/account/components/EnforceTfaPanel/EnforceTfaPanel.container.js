f019cfcb0982e1cd7835d184a19596e677d8a39e
shrirupa <shrirupa.chowdhury@sparkpost.com>
2020-10-30 16:25:50 -0400

    [AC-1775] Removed the condition for starter plans

--- src/pages/account/components/EnforceTfaPanel/EnforceTfaPanel.container.js
+++ src/pages/account/components/EnforceTfaPanel/EnforceTfaPanel.container.js
@@ -1,29 +1,20 @@
 import { connect } from 'react-redux';
 import { showAlert } from 'src/actions/globalAlert';
-
 import {
   getAccountSingleSignOnDetails,
   updateAccountSingleSignOn,
 } from 'src/actions/accountSingleSignOn';
-
 import { getTfaStatus } from 'src/actions/tfa';
-
 import { logout } from 'src/actions/auth';
-
 import { update as updateAccount } from 'src/actions/account';
-
 import EnforceTfaPanel from './EnforceTfaPanel';
-import { getBundleTierByPlanCode } from 'src/selectors/accountBillingInfo';
 
 const mapStateToProps = state => {
   const { accountSingleSignOn, account, tfa } = state;
   return {
     loading: accountSingleSignOn.loading || tfa.enabled === null,
     ssoEnabled: accountSingleSignOn.enabled,
-    tfaRequired:
-      getBundleTierByPlanCode(state) === 'starter' && account.tfa_required
-        ? false
-        : account.tfa_required,
+    tfaRequired: account.tfa_required,
     tfaUpdatePending: account.updateLoading,
     tfaEnabled: tfa.enabled,
   };
