181f11ee68ebcceff375bd6a242ece9e7877ad9b
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-11-18 15:17:27 -0500

    SA-1773 - Fix unit test failure due to indexed import

--- src/pages/reportBuilder/components/SavedReportsSection/SaveReportModal.js
+++ src/pages/reportBuilder/components/SavedReportsSection/SaveReportModal.js
@@ -17,8 +17,7 @@ import { createReport, updateReport, getReports } from 'src/actions/reports';
 import { showAlert } from 'src/actions/globalAlert';
 import { getMetricsFromKeys } from 'src/helpers/metrics';
 import { useReportBuilderContext } from '../../context/ReportBuilderContext';
-import { ActiveFilters } from '../index';
-
+import ActiveFilters from '../ActiveFilters';
 import { formatDateTime, relativeDateOptionsIndexed } from 'src/helpers/date';
 
 const DateRange = ({ to, from, relativeRange }) => {
