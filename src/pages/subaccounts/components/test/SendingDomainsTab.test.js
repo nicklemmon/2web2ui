dc9d5c9eec848d72915ea1eca62ed082f77f7452
shrirupa <shrirupa.chowdhury@sparkpost.com>
2020-11-19 11:38:50 -0500

    [FE-1244] Fixed the bug

--- src/pages/subaccounts/components/test/SendingDomainsTab.test.js
+++ src/pages/subaccounts/components/test/SendingDomainsTab.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { SendingDomainsTab, getRowData } from '../SendingDomainsTab';
+import { HibanaConsumer } from 'src/context/HibanaContext';
 
 describe('SendingDomainsTab', () => {
   const defaultProps = {
@@ -15,11 +16,14 @@ describe('SendingDomainsTab', () => {
     loading: false,
   };
 
-  const subject = props => shallow(<SendingDomainsTab {...defaultProps} {...props} />);
-
+  const subject = props =>
+    shallow(<SendingDomainsTab {...defaultProps} {...props} />, {
+      context: { isHibanaEnabled: false },
+    });
   it('should load domains in tab', () => {
-    const wrapper = subject();
-
+    const wrapper = subject()
+      .find(HibanaConsumer)
+      .renderProp('children')();
     expect(wrapper).toHaveTextContent('Sending Domains assigned to this subaccount.');
     expect(wrapper.find('TableCollection')).toHaveProp('rows', defaultProps.domains);
   });
@@ -32,7 +36,9 @@ describe('SendingDomainsTab', () => {
   });
 
   it('should show empty message when 0 domains exist', () => {
-    const wrapper = subject({ domains: [] });
+    const wrapper = subject({ domains: [] })
+      .find(HibanaConsumer)
+      .renderProp('children')();
 
     expect(wrapper).toHaveTextContent(
       'This subaccount has no sending domains assigned to it. You can assign an existing one, or create a new one.',
