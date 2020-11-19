90e939343b741d2ea9d4dd867e899f6a9ba11d32
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-11-19 11:12:27 -0500

    kill-the-og-theme-for-fun - Remove functional tests

--- src/__func__/abTesting.test.js
+++
@@ -1,6 +0,0 @@
-import mountRoute from 'src/__testHelpers__/mountRoute';
-
-test('A/B testing edit page', async () => {
-  const { wrapper } = await mountRoute('/ab-testing/101/1');
-  expect(wrapper.find('Page')).toExist();
-});
