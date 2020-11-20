acc0f0ce3e419884da8fbb65d406982fd97ec52e
Neal Stanard <nstanard@gmail.com>
2020-11-20 12:58:10 -0600

    FE-1244 - Mocking the consumer layer with a function that returns an object

--- src/pages/dashboard/components/tests/GettingStartedGuide.test.js
+++ src/pages/dashboard/components/tests/GettingStartedGuide.test.js
@@ -20,7 +20,20 @@ const defaultProps = {
 };
 
 describe('GettingStartedGuide full', () => {
-  const subject = (props, renderFn = render) =>
+  const hibanaOnSubject = (props, renderFn = render) =>
+    renderFn(
+      <TestApp isHibanaEnabled={true}>
+        <GettingStartedGuide {...defaultProps} {...props} />
+      </TestApp>,
+    );
+
+  it('should navigate to /domains/create Add Sending Domain is clicked - hibana', () => {
+    const { queryByText } = hibanaOnSubject({ onboarding: { active_step: "Let's Code" } });
+    userEvent.click(queryByText('Add Sending Domain'));
+    expect(defaultProps.history.push).toHaveBeenCalledWith(`/domains/create`);
+  });
+
+  const hibanaOffSubject = (props, renderFn = render) =>
     renderFn(
       <TestApp>
         <GettingStartedGuide {...defaultProps} {...props} />
@@ -28,7 +41,7 @@ describe('GettingStartedGuide full', () => {
     );
 
   it('should render ShowMeSparkpostStep inside "Start Sending with SparkPost" Expandable and this Expandable is open by default', () => {
-    const { queryByText, queryAllByTestId } = subject({
+    const { queryByText, queryAllByTestId } = hibanaOffSubject({
       onboarding: { active_step: 'Show Me SparkPost' },
     });
 
@@ -42,7 +55,7 @@ describe('GettingStartedGuide full', () => {
   });
 
   it('should render LetsCodeStep inside SparkPost Analytics Expandable', () => {
-    const { queryByText, queryAllByTestId } = subject({
+    const { queryByText, queryAllByTestId } = hibanaOffSubject({
       onboarding: { active_step: "Let's Code" },
     });
     const expandables = queryAllByTestId('expandable-toggle');
@@ -56,7 +69,7 @@ describe('GettingStartedGuide full', () => {
   });
 
   it('should not render the "Start Sending with SparkPost" Expandable when user does not have grants to manageKeys or manageSendingDomains', () => {
-    const { queryByText, queryAllByTestId } = subject({
+    const { queryByText, queryAllByTestId } = hibanaOffSubject({
       canManageKeys: false,
       canManageSendingDomains: false,
       onboarding: { active_step: "Let's Code" },
@@ -68,7 +81,7 @@ describe('GettingStartedGuide full', () => {
   });
 
   it('should navigate to templates page when Send a Test Email button is clicked', () => {
-    const { queryByText } = subject({ onboarding: { active_step: 'Show Me SparkPost' } });
+    const { queryByText } = hibanaOffSubject({ onboarding: { active_step: 'Show Me SparkPost' } });
     userEvent.click(queryByText('Send Test Email'));
     expect(defaultProps.history.push).toHaveBeenCalledWith(
       `/templates?appcue=${GUIDE_IDS.SEND_TEST_EMAIL}`,
@@ -76,7 +89,7 @@ describe('GettingStartedGuide full', () => {
   });
 
   it('should navigate to summary report when Explore Analytics button is clicked', () => {
-    const { getAllByText } = subject({ onboarding: { active_step: 'Show Me SparkPost' } });
+    const { getAllByText } = hibanaOffSubject({ onboarding: { active_step: 'Show Me SparkPost' } });
     userEvent.click(getAllByText('Explore Analytics')[1]);
 
     expect(defaultProps.history.push).toHaveBeenCalledWith(`/reports/summary`, {
@@ -85,7 +98,7 @@ describe('GettingStartedGuide full', () => {
   });
 
   it('should navigate to events page when Check Out Events button is clicked', () => {
-    const { getAllByText } = subject({ onboarding: { active_step: 'Show Me SparkPost' } });
+    const { getAllByText } = hibanaOffSubject({ onboarding: { active_step: 'Show Me SparkPost' } });
     userEvent.click(getAllByText('Check Out Events')[1]);
 
     expect(defaultProps.history.push).toHaveBeenCalledWith(`/reports/message-events`, {
@@ -94,7 +107,7 @@ describe('GettingStartedGuide full', () => {
   });
 
   it('should navigate to users page when Invite a Collaborator is clicked', () => {
-    const { queryByText } = subject({ onboarding: { active_step: 'Show Me SparkPost' } });
+    const { queryByText } = hibanaOffSubject({ onboarding: { active_step: 'Show Me SparkPost' } });
     userEvent.click(queryByText('Invite a Collaborator'));
     expect(defaultProps.history.push).toHaveBeenCalledWith(`/account/users`);
     expect(defaultProps.setAccountOption).toHaveBeenCalledWith('onboarding', {
@@ -103,7 +116,7 @@ describe('GettingStartedGuide full', () => {
   });
 
   it('should have an external link to developer docs', () => {
-    const { queryByText } = subject({ onboarding: { active_step: "Let's Code" } });
+    const { queryByText } = hibanaOffSubject({ onboarding: { active_step: "Let's Code" } });
     userEvent.click(queryByText('View Developer Docs'));
     expect(defaultProps.setAccountOption).toHaveBeenCalledWith('onboarding', {
       view_developer_docs_completed: true,
@@ -111,7 +124,7 @@ describe('GettingStartedGuide full', () => {
   });
 
   it("should route to API key page from Let's Code list", () => {
-    const { queryByText } = subject({
+    const { queryByText } = hibanaOffSubject({
       onboarding: { active_step: "Let's Code" },
       hasApiKeysForSending: true,
     });
@@ -120,25 +133,10 @@ describe('GettingStartedGuide full', () => {
   });
 
   it('should navigate to sending domains page when Add Sending Domain is clicked', () => {
-    const { queryByText } = subject({ onboarding: { active_step: "Let's Code" } });
+    const { queryByText } = hibanaOffSubject({ onboarding: { active_step: "Let's Code" } });
     userEvent.click(queryByText('Add Sending Domain'));
     expect(defaultProps.history.push).toHaveBeenCalledWith(`/account/sending-domains`, {
       triggerGuide: true,
     });
-  });
-});
-
-describe('GettingStartedGuide full hibana', () => {
-  const subject = (props, renderFn = render) =>
-    renderFn(
-      <TestApp isHibanaEnabled={true}>
-        <GettingStartedGuide {...defaultProps} {...props} />
-      </TestApp>,
-    );
-
-  it('should navigate to /domains/create Add Sending Domain is clicked - hibana', () => {
-    const { queryByText } = subject({ onboarding: { active_step: "Let's Code" } });
-    userEvent.click(queryByText('Add Sending Domain'));
-    expect(defaultProps.history.push).toHaveBeenCalledWith(`/domains/create`);
   });
 });
--- src/pages/subaccounts/components/test/SendingDomainsTab.test.js
+++ src/pages/subaccounts/components/test/SendingDomainsTab.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { SendingDomainsTab, getRowData } from '../SendingDomainsTab';
-import { HibanaConsumer } from 'src/context/HibanaContext';
+import * as HibanaContext from 'src/context/HibanaContext';
+
+const mockConsumer = jest.fn();
 
 describe('SendingDomainsTab', () => {
+  beforeEach(() => {
+    mockConsumer.mockReset();
+  });
+
   const defaultProps = {
     domains: [
       {
@@ -16,29 +22,29 @@ describe('SendingDomainsTab', () => {
     loading: false,
   };
 
-  const subject = props =>
-    shallow(<SendingDomainsTab {...defaultProps} {...props} />, {
-      context: { isHibanaEnabled: false },
-    });
+  const subject = props => shallow(<SendingDomainsTab {...defaultProps} {...props} />);
+
   it('should load domains in tab', () => {
+    mockConsumer.mockReturnValue({ isHibanaEnabled: false });
     const wrapper = subject()
-      .find(HibanaConsumer)
+      .find(HibanaContext.HibanaStateContext.Consumer)
       .renderProp('children')();
     expect(wrapper).toHaveTextContent('Sending Domains assigned to this subaccount.');
     expect(wrapper.find('TableCollection')).toHaveProp('rows', defaultProps.domains);
   });
 
   it('should show panel loading while loading domains', () => {
+    mockConsumer.mockReturnValue({ isHibanaEnabled: false });
     const wrapper = subject({ loading: true });
-
     expect(wrapper.find('Loading')).toExist();
     expect(wrapper.find('TableCollection')).not.toExist();
   });
 
   it('should show empty message when 0 domains exist', () => {
+    mockConsumer.mockReturnValue({ isHibanaEnabled: false });
     const wrapper = subject({ domains: [] })
-      .find(HibanaConsumer)
-      .renderProp('children')();
+      .find(HibanaContext.HibanaStateContext.Consumer)
+      .renderProp('children')(mockConsumer());
 
     expect(wrapper).toHaveTextContent(
       'This subaccount has no sending domains assigned to it. You can assign an existing one, or create a new one.',
@@ -46,7 +52,14 @@ describe('SendingDomainsTab', () => {
     expect(wrapper.find('PageLink')).toHaveProp('to', '/account/sending-domains');
   });
 
+  it('should show a link to /domains/list/sending when hibana is enabled', () => {
+    mockConsumer.mockReturnValue({ isHibanaEnabled: true });
+    const wrapper = subject({ domains: [] }).renderProp('children')(mockConsumer());
+    expect(wrapper.find('PageLink')).toHaveProp('to', '/domains/list/sending');
+  });
+
   it('getRowData', () => {
+    mockConsumer.mockReturnValue({ isHibanaEnabled: true });
     expect(getRowData({ domain: 'foo.com' })).toMatchSnapshot();
   });
 });
