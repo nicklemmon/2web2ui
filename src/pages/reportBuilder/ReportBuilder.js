d2cbc5f07088244f2d060fa76cd7ad807f3ef58d
shrirupa <shrirupa.chowdhury@sparkpost.com>
2020-12-04 10:38:48 -0500

    [FE-1266] fixing runaway useEffect

--- src/pages/reportBuilder/ReportBuilder.js
+++ src/pages/reportBuilder/ReportBuilder.js
@@ -54,7 +54,7 @@ export function ReportBuilder({
   }, [reportOptions.metrics]);
 
   useEffect(() => {
-    if (reportOptions.isReady && !isEmpty && !(chart.groupBy === 'aggregate')) {
+    if (reportOptions.isReady && !isEmpty && chart.groupBy !== 'aggregate') {
       let updates = {
         ...reportOptions,
         filters: reportOptions.filters,
@@ -74,7 +74,8 @@ export function ReportBuilder({
 
       getTableData({ params, metrics: merged.metrics });
     }
-  }, [reportOptions, isEmpty, chart.groupBy, chart, getTableData]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [reportOptions, isEmpty, getTableData]);
 
   useEffect(() => {
     getSubscription();
